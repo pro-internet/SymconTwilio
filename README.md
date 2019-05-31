44282d93f6e97d6e860cc822ffe2b8a668773627
PROINTERNET\Noah <n.wilhelm@prointernet.de>
2019-05-31 09:42:00 +0200

    - Init

---
+++ README.md
@@ -0,0 +1,5 @@
+# SymconTwilio
+## Benutzung
+* Konfiguration über Instanzformular.
+* Twilio_GetClient() -> erstellt neuen Twilio Client
+* Dokumentation zu Twilio API Client: https://www.twilio.com/docs/libraries/php
---
+++ SymconTwilio/module.php
@@ -0,0 +1,47 @@
+<?
+
+    // Klassendefinition
+    class Twilio extends IPSModule {
+ 
+        // Der Konstruktor des Moduls
+        // Überschreibt den Standard Kontruktor von IPS
+        public function __construct($InstanceID) {
+            // Diese Zeile nicht löschen
+            parent::__construct($InstanceID);
+
+            // Selbsterstellter Code
+        }
+ 
+        // Überschreibt die interne IPS_Create($id) Funktion
+        public function Create() {
+
+            parent::Create();
+
+
+           $this->RegisterPropertyString("sid", "");
+           $this->RegisterPropertyString("token", "");
+ 
+        }
+
+        public function GetClient() {
+
+            echo __DIR__;
+
+        }
+
+ 
+        public function ApplyChanges() {
+            parent::ApplyChanges();
+        }
+
+        public function Destroy () {
+
+            parent::Destroy();
+
+        }
+
+
+    }
+
+
+?>
